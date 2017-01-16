b74bc9184d15492ee6f402f98534b8b07ad1c756
jediazzi <jefferson_diazzi@hotmail.com>
2017-01-16 00:08:05 -0200

    Jogo mega-sena

---
+++ mega-sena.rb
@@ -0,0 +1,11 @@
+a =(0..60).to_a.shuffle
+
+numero1 = a.pop.to_s
+numero2 = a.pop.to_s
+numero3 = a.pop.to_s
+numero4 = a.pop.to_s
+numero5 = a.pop.to_s
+numero6 = a.pop.to_s
+
+puts "Quer jogar na Mega-Sena??? Aqui estao seus numeros!!!"
+puts numero1 + " - " + numero2 + " - " + numero3 + " - " + numero4 + " - " + numero5 + " - " + numero6
