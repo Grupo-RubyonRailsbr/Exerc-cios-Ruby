f94eccda3a667a04f170051311f9cc34213c984c
jefesonbezerra <jefesonbezerra@gmail.com>
2017-01-15 22:11:01 -0300

    Add files via upload

---
+++ exercicio_1_concatenacao.rb
@@ -0,0 +1,4 @@
+#Exercício 1 - concatenação: Apostila caelum-ruby-on-rails-rr71 
+puts "Digite o nome do restaurante: "
+nome = gets
+print "Nome do restaurante: " + nome.capitalize
---
+++ exercicio_2_interpolacao.rb
@@ -0,0 +1,7 @@
+#Exercício 2 - interpolação Apostila caelum-ruby-on-rails-rr71 
+# encoding: UTF-8 
+
+puts "Digite o á nome do restaurante: "
+nome = gets
+print "O nome do restaurante é: "
+puts nome.capitalize
